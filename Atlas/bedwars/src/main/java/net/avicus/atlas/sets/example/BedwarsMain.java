a9d6727d107dffc605ef877fec00ad4eb73e8be9
Jason Ho <jasoryeh@gmail.com>
2019-03-03 11:44:55 -0800

    Fix compilation issues

--- Atlas/bedwars/src/main/java/net/avicus/atlas/sets/example/BedwarsMain.java
+++ Atlas/bedwars/src/main/java/net/avicus/atlas/sets/example/BedwarsMain.java
@@ -3,15 +3,8 @@ package net.avicus.atlas.sets.example;
 import lombok.Getter;
 import lombok.Setter;
 import net.avicus.atlas.Atlas;
-import net.avicus.atlas.GameType;
 import net.avicus.atlas.external.ModuleSet;
-import net.avicus.atlas.map.AtlasMap;
-import net.avicus.atlas.map.AtlasMapFactory;
-import net.avicus.atlas.match.Match;
-import net.avicus.magma.util.MapGenre;
 
-import java.util.Optional;
-import java.util.Set;
 import java.util.logging.Logger;
 
 public class BedwarsMain extends ModuleSet {
@@ -28,19 +21,6 @@ public class BedwarsMain extends ModuleSet {
         instance = this;
 
         this.logger.info("Enabled bedwars set.");
-
-        AtlasMapFactory.TYPE_DETECTORS.add(new AtlasMap.TypeDetector() {
-
-            @Override
-            public Optional<MapGenre> detectGenre(Match match) {
-                return match.hasModule()
-            }
-
-            @Override
-            public Set<GameType> detectGameTypes(Match match) {
-                return null;
-            }
-        });
     }
 
     @Override
