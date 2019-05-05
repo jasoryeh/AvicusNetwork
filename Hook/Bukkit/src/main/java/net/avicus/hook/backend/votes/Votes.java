6d1ed50b8c27fa422be2739f9b79ff1a468e7252
Jason Ho <jasoryeh@gmail.com>
2019-05-05 14:48:34 -0700

    Check for Votifier before enabling backend processing for votes

--- Hook/Bukkit/src/main/java/net/avicus/hook/backend/votes/Votes.java
+++ Hook/Bukkit/src/main/java/net/avicus/hook/backend/votes/Votes.java
@@ -1,10 +1,19 @@
 package net.avicus.hook.backend.votes;
 
+import net.avicus.hook.HookPlugin;
 import net.avicus.hook.utils.Events;
 
 public class Votes {
 
     public static void init() {
-        Events.register(new VoteListener());
+        try {
+            Class.forName("com.vexsoftware.votifier.model.Vote");
+
+            Events.register(new VoteListener());
+        } catch (ClassNotFoundException e) {
+            HookPlugin.getInstance().getLogger().info("Vote not found. Not enabling the listener.");
+
+            e.printStackTrace();
+        }
     }
 }
