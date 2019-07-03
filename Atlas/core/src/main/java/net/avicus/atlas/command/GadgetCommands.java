aece9a7b26ab6bfe764afe37a510eeab508c967d
Jason Ho <jasoryeh@gmail.com>
2019-07-03 12:14:43 -0700

    Allow command to work with offline players

--- Atlas/core/src/main/java/net/avicus/atlas/command/GadgetCommands.java
+++ Atlas/core/src/main/java/net/avicus/atlas/command/GadgetCommands.java
@@ -12,11 +12,8 @@ import net.avicus.magma.module.gadgets.Gadgets;
 import net.avicus.magma.module.gadgets.crates.CrateGadget;
 import net.avicus.magma.module.gadgets.crates.KeyGadget;
 import net.avicus.magma.module.gadgets.crates.TypeManager;
-import net.avicus.magma.network.user.Users;
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 import java.util.Date;
 import java.util.Optional;
@@ -25,29 +22,26 @@ import java.util.UUID;
 public class GadgetCommands {
     @Command(aliases = {"gadget", "gadgets"}, desc = "Manage Atlas gadgets.", min = 0, max = -1)
     public static void gadget(CommandContext cmd, final CommandSender sender) {
-        sender.sendMessage("/gadget [give] [key|crate|maptoken] [uuid or username] [other e.g. type/alpha/beta/etc.]");
 
         User user = null;
         if (cmd.argsLength() >= 3) {
             String arg3 = cmd.getString(2);
             if (arg3.contains("-")) {
-                Optional<User> oUser = Users.user(UUID.fromString(arg3));
+                Optional<User> oUser = Magma.get().database().getUsers().findByUuid(UUID.fromString(arg3));
                 if (oUser.isPresent()) {
                     user = oUser.get();
                 }
             }
 
-            Player player = Bukkit.getPlayer(arg3);
-            if (player != null) {
-                Optional<User> user1 = Users.user(player.getUniqueId());
-                if (user1.isPresent()) {
-                    user = user1.get();
-                }
+            Optional<User> oUser = Magma.get().database().getUsers().findByName(arg3);
+            if (oUser.isPresent()) {
+                user = oUser.get();
             }
         }
 
         if (user == null) {
-            sender.sendMessage("Invalid user.");
+            sender.sendMessage(ChatColor.RED + "/gadget [give] [key|crate|maptoken] [uuid or username] [other e.g. type/alpha/beta/etc.]");
+            sender.sendMessage("Invalid user specified.");
             return;
         }
 
@@ -55,16 +49,17 @@ public class GadgetCommands {
             if (cmd.getString(1).equalsIgnoreCase("key")) {
                 Gadget token = new KeyGadget(TypeManager.getType(cmd.getString(3)));
                 Magma.get().getMm().get(Gadgets.class).createBackpackGadget(user, token.defaultContext(), true, new Date());
-                sender.sendMessage("Successfully added.");
+                sender.sendMessage("Successfully added key.");
             } else if (cmd.getString(1).equalsIgnoreCase("crate")) {
                 Gadget token = new CrateGadget(TypeManager.getType(cmd.getString(3)));
                 Magma.get().getMm().get(Gadgets.class).createBackpackGadget(user, token.defaultContext(), true, new Date());
-                sender.sendMessage("Successfully added.");
+                sender.sendMessage("Successfully added crate.");
             } else if (cmd.getString(1).equalsIgnoreCase("maptoken")) {
                 Gadget token = new MapSetNextToken();
                 Magma.get().getMm().get(Gadgets.class).createBackpackGadget(user, token.defaultContext(), true, new Date());
-                sender.sendMessage("Successfully added.");
+                sender.sendMessage("Successfully added map token.");
             } else {
+                sender.sendMessage(ChatColor.RED + "/gadget [give] [key|crate|maptoken] [uuid or username] [other e.g. type/alpha/beta/etc.]");
                 sender.sendMessage(ChatColor.RED + "Invalid/Unsupported gadget.");
             }
         }
